15f8699b8924bfe6da23d3a63ec3049a9052305a
George Toothman <george.toothman@flatironschool.com>
2015-10-07 10:54:49 -0400

    remove helpers for org service obj

--- app/services/create_organization.rb
+++ app/services/create_organization.rb
@@ -1,15 +1,11 @@
-require './app/helpers/organizations_helper'
-
 class CreateOrganization
-  include OrganizationsHelper
   include Change::Resources
   attr_accessor :client, :organization, :loaded_organization, :name, :website, :change_org_url
 
   def initialize
     @client = Change::Requests::Client.new({ :api_key => ENV["CHANGE_KEY"], :secret_token => ENV["CHANGE_SECRET"] })
     @organization = Organization.new(@client)
-    binding.pry
-    @loaded_organization = @organization.load(pick_organization)
+    @loaded_organization = @organization.load.pick_organization(url_array)
     @name = @loaded_organization['name']
     @website = @loaded_organization['website'] 
     @change_org_url = @loaded_organization['organization_url']
@@ -17,9 +13,11 @@ class CreateOrganization
 
   private
 
-  def pick_organization
-    return_organizations_array.sample
+  url_array = ["https://www.change.org/organizations/goodli", "https://www.change.org/organizations/duoduo_animal_welfare_project","https://www.change.org/organizations/population_connection_action_fund", "https://www.change.org/u/356393312", "https://www.change.org/organizations/mercy_for_animals"]
+
+  def pick_organization(url_array)
+    url_array.sample
   end
 end
 
-CreateOrganization.new(client, return_organizations_array).call
+test = CreateOrganization.new
