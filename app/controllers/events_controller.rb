4fb2b86c01a5dadad0c005a7b9e1d3ccc4514601
George Toothman <george.toothman@flatironschool.com>
2015-10-07 11:01:11 -0400

    test org object in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new
-    @organization = CreateOrganization.new(client, return_organizations_array).call
+    @organization = CreateOrganization.new
     @event.title = params["title"]
     @event.description = params["description"]
     @event.users << current_user
