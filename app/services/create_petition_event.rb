3c7b4b4a7c85808cb17a1508806a73bf2dbce38a
George Toothman <george.toothman@flatironschool.com>
2015-10-08 14:31:15 -0400

    add change api

--- app/services/create_petition_event.rb
+++ app/services/create_petition_event.rb
@@ -1,9 +1,8 @@
 class CreatePetitionEvent
   include Change::Resources
-  attr_accessor :loaded_organization
   BASE_URL = "https://www.change.org/organizations/"
-  ORG_URL = "https://api.change.org/v1/organizations"
-  ORGS = [long array of some orgs]
+  # ORG_URL = "https://api.change.org/v1/organizations"
+  ORGS = ["duoduo_animal_welfare_project", "goodli", "glaad", "population_connection_action_fund", "lcv"]
   
   def initialize
     organization = ORGS.sample
@@ -12,16 +11,25 @@ class CreatePetitionEvent
     @organization = Organization.new(@client)
     org_id = @organization.get_id(BASE_URL + "#{organization}")
     loaded_org = @organization.load(org_id)
+    binding.pry
   end
 
   def get_petitions
-    @organization.petitions.load
+    sample_petition = @organization.petitions.load
     # look at the result object. Is it an array of petitions? What data comes w/ each petition? What data will we take to make 
     #   new Event objects
+    sample_title = sample_petition[0]["title"]
+    sample_url = sample_petition[0]["url"]
+    new_event = Event.new
+    new_event.title = sample_title
+    new_event.save
+    new_event.description = sample_url
+    new_event.description.save
+    binding.pry
   end
 
   def make_events_from_petitions
-    get_petitions.each do |make the eventslasdjfaw|
+    # get_petitions.each do |make the eventslasdjfaw|
       # the result of this method is several newly created and saved events
   end
 end
--- spec/services/create_petition_event_spec.rb
+++ spec/services/create_petition_event_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 describe CreatePetitionEvent do 
   describe "initialize" do
     it "should be an event from the change api" do
-      service_object = CreatePetitionEvent.new("duoduo_animal_welfare_project")
+      service_object = CreatePetitionEvent.new
       expect(service_object.loaded_organization).to be_a Hash
     end
   end
